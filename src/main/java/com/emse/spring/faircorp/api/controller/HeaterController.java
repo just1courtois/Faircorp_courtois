08e7089abe2192b33e7fd94bb28b0c43d92f91e9
just1courtois <59601373+just1courtois@users.noreply.github.com>
2021-11-29 19:03:52 +0100

    Update HeaterController.java

--- src/main/java/com/emse/spring/faircorp/api/controller/HeaterController.java
+++ src/main/java/com/emse/spring/faircorp/api/controller/HeaterController.java
@@ -15,79 +15,79 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/heaters")
 @Transactional
 @CrossOrigin
-public class HeaterController {
-    private final HeaterDao heaterDao;
-    private final RoomDao roomDao;
+    public class HeaterController {
+        private final HeaterDao heaterDao;
+        private final RoomDao roomDao;
 
-    public HeaterController(HeaterDao heaterDao, RoomDao roomDao) {
-        this.heaterDao = heaterDao;
-        this.roomDao = roomDao;
-    }
-
-    @GetMapping // (5)
-    public List<HeaterDto> findAll() {
-        return heaterDao.findAll().stream().map(HeaterDto::new).collect(Collectors.toList());  // (6)
-    }
-
-    @GetMapping(path = "/room/{room_id}") // (5)
-    public List<HeaterDto> findHeaterByRoom(@PathVariable Long room_id) {
-        return heaterDao.findHeatersByRoom(room_id).stream().map(HeaterDto::new).collect(Collectors.toList());  // (6)
-    }
-
-    @GetMapping(path = "/{id}")
-    public HeaterDto findById(@PathVariable Long id) {
-        return heaterDao.findById(id).map(HeaterDto::new).orElse(null); // (7)
-    }
-
-
-    @PutMapping(path = "/{id}/switch")
-    public HeaterDto switchStatus(@PathVariable Long id) {
-        Heater heater = heaterDao.findById(id).orElseThrow(IllegalArgumentException::new);
-        heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.ON ? HeaterStatus.OFF: HeaterStatus.ON);
-        return new HeaterDto(heater);
-    }
-
-    @PutMapping(path = "/switchHeatersOn/{room_id}")
-    public HeaterDto switchHeatersStatusToOn(@PathVariable Long room_id) {
-        List<Heater> heaters = heaterDao.findHeatersByRoom(room_id);
-        for (Heater heater : heaters) {
-            heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.OFF? HeaterStatus.ON: HeaterStatus.ON);
+        public HeaterController(HeaterDao heaterDao, RoomDao roomDao) {
+            this.heaterDao = heaterDao;
+            this.roomDao = roomDao;
         }
-        return new HeaterDto(heaters);
 
-    }
-
-    @PutMapping(path = "/switchHeatersOff/{room_id}")
-    public HeaterDto switchHeatersStatusToOff(@PathVariable Long room_id) {
-        List<Heater> heaters = heaterDao.findHeatersByRoom(room_id);
-        for (Heater heater : heaters) {
-            heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.ON? HeaterStatus.OFF: HeaterStatus.OFF);
+        @GetMapping // (5)
+        public List<HeaterDto> findAll() {
+            return heaterDao.findAll().stream().map(HeaterDto::new).collect(Collectors.toList());  // (6)
         }
-        return new HeaterDto(heaters);
 
-    }
-
-
-
-    @PostMapping // (8)
-    public HeaterDto create(@RequestBody HeaterDto dto) {
-        // HeaterDto must always contain the window room
-        Room room = roomDao.getById(dto.getRoomId());
-        Heater heater = null;
-        // On creation id is not defined
-        if (dto.getId() == null) {
-            heater = heaterDao.save(new Heater(dto.getName(), dto.getHeaterStatus(), room));
+        @GetMapping(path = "/room/{room_id}") // (5)
+        public List<HeaterDto> findHeaterByRoom(@PathVariable Long room_id) {
+            return heaterDao.findHeatersByRoom(room_id).stream().map(HeaterDto::new).collect(Collectors.toList());  // (6)
         }
-        else {
-            heater = heaterDao.getById(dto.getId());  // (9)
-            heater.setHeaterStatus(dto.getHeaterStatus());
+
+        @GetMapping(path = "/{id}")
+        public HeaterDto findById(@PathVariable Long id) {
+            return heaterDao.findById(id).map(HeaterDto::new).orElse(null); // (7)
         }
-        return new HeaterDto(heater);
-    }
 
 
-    @DeleteMapping(path = "/{id}")
-    public void delete(@PathVariable Long id) {
-        heaterDao.deleteById(id);
+        @PutMapping(path = "/{id}/switch")
+        public HeaterDto switchStatus(@PathVariable Long id) {
+            Heater heater = heaterDao.findById(id).orElseThrow(IllegalArgumentException::new);
+            heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.ON ? HeaterStatus.OFF: HeaterStatus.ON);
+            return new HeaterDto(heater);
+        }
+
+        @PutMapping(path = "/switchHeatersOn/{room_id}")
+        public HeaterDto switchHeatersStatusToOn(@PathVariable Long room_id) {
+            List<Heater> heaters = heaterDao.findHeatersByRoom(room_id);
+            for (Heater heater : heaters) {
+                heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.OFF? HeaterStatus.ON: HeaterStatus.ON);
+            }
+            return new HeaterDto(heaters);
+
+        }
+
+        @PutMapping(path = "/switchHeatersOff/{room_id}")
+        public HeaterDto switchHeatersStatusToOff(@PathVariable Long room_id) {
+            List<Heater> heaters = heaterDao.findHeatersByRoom(room_id);
+            for (Heater heater : heaters) {
+                heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.ON? HeaterStatus.OFF: HeaterStatus.OFF);
+            }
+            return new HeaterDto(heaters);
+
+        }
+
+
+
+        @PostMapping // (8)
+        public HeaterDto create(@RequestBody HeaterDto dto) {
+            // HeaterDto must always contain the window room
+            Room room = roomDao.getById(dto.getRoomId());
+            Heater heater = null;
+            // On creation id is not defined
+            if (dto.getId() == null) {
+                heater = heaterDao.save(new Heater(dto.getName(), dto.getHeaterStatus(), room));
+            }
+            else {
+                heater = heaterDao.getById(dto.getId());  // (9)
+                heater.setHeaterStatus(dto.getHeaterStatus());
+            }
+            return new HeaterDto(heater);
+        }
+
+
+        @DeleteMapping(path = "/{id}")
+        public void delete(@PathVariable Long id) {
+            heaterDao.deleteById(id);
+        }
     }
-}
