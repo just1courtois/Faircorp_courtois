8076ae5ef79c96bc7232165055000e88b2d5f5d2
just1courtois <59601373+just1courtois@users.noreply.github.com>
2021-12-09 10:01:14 +0100

    crossOrigin

--- src/main/java/com/emse/spring/faircorp/api/controller/BuildingController.java
+++ src/main/java/com/emse/spring/faircorp/api/controller/BuildingController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/buildings")
 @Transactional
+@CrossOrigin
 public class BuildingController {
     private final BuildingDao buildingDao;
     private final RoomDao roomDao;
